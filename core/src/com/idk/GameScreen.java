b394f63b2d2073bb4f623f6af1f496709eba58e6
dylanjones <4dylan.jones@gmail.com>
2022-02-08 19:49:57 -0500

    Shrubs

--- core/src/com/idk/GameScreen.java
+++ core/src/com/idk/GameScreen.java
@@ -11,14 +11,21 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.AnimatedTiledMapTile;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.util.Iterator;
 
 public class GameScreen implements Screen
 {
@@ -35,25 +42,41 @@ public class GameScreen implements Screen
     private TiledMap map;
     private TiledMapTileLayer collisionLayer;
 
+    // animated tiles
+    Iterator<TiledMapTile> tiles;
+
+    Array<StaticTiledMapTile> streamTilesH;
+    Array<StaticTiledMapTile> streamTilesV;
+    Array<StaticTiledMapTile> streamTilesUL;
+    Array<StaticTiledMapTile> streamTilesUR;
+    Array<StaticTiledMapTile> streamTilesDL;
+    Array<StaticTiledMapTile> streamTilesDR;
+    Array<StaticTiledMapTile> shrubTiles;
+
+    AnimatedTiledMapTile animatedStreamTileH;
+    AnimatedTiledMapTile animatedStreamTileV;
+    AnimatedTiledMapTile animatedStreamTileUL;
+    AnimatedTiledMapTile animatedStreamTileUR;
+    AnimatedTiledMapTile animatedStreamTileDL;
+    AnimatedTiledMapTile animatedStreamTileDR;
+    AnimatedTiledMapTile animatedShrubTile;
+
 
     public GameScreen()
     {
         camera = new OrthographicCamera(10, 10);
-
-
-
         viewport = new StretchViewport(200,200,camera);
 
-
-
-        player = new Player(200, 200, 16,32, 70, 2);
+        player = new Player(300, 300, 16,32, 70, 2);
 
         batch = new SpriteBatch();
 
-        map = new TmxMapLoader().load("BorderMap.tmx");
+        map = new TmxMapLoader().load("ShrubMap.tmx");
         tmr = new OrthogonalTiledMapRenderer(map, batch);
         collisionLayer = (TiledMapTileLayer)map.getLayers().get(0);
+
     }
+
 
 
     @Override
@@ -63,21 +86,179 @@ public class GameScreen implements Screen
         camera.position.y = player.boundingBox.y + player.boundingBox.height/2;
         camera.update();
 
+        animateTiles();
+
         tmr.render();
 
         batch.begin();
 
-        player.draw(batch, deltaTime);
+        player.draw(batch, deltaTime, player.movementSpeed);
 
         tmr.setView(camera);
 
         detectInput(deltaTime);
+
+        checkForShrubs();
 
         batch.end();
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
         {
             Gdx.app.exit();
+        }
+    }
+
+    private void checkForShrubs()
+    {
+        int x = (int)(player.boundingBox.x + player.boundingBox.width/2)/16;
+        int y = (int)(player.boundingBox.y + player.boundingBox.height/3)/16;
+        TiledMapTileLayer layer = (TiledMapTileLayer)map.getLayers().get(0);
+        if (layer.getCell(x, y).getTile().getProperties().containsKey("Animation"))
+        {
+            if (layer.getCell(x, y).getTile().getProperties().get("Animation").equals("Shrub"))
+            {
+                final TiledMapTileLayer.Cell cell = layer.getCell(x, y);
+                cell.setTile(shrubTiles.get(1));
+
+                Timer timer = new Timer();
+                timer.scheduleTask(new Timer.Task()
+                {
+                    @Override
+                    public void run()
+                    {
+                        cell.setTile(shrubTiles.get(0));
+                    }
+                }, .5f);
+
+            }
+        }
+    }
+
+    private void turnOffShrub(TiledMapTileLayer layer, int x, int y)
+    {
+        if (Gdx.input.isKeyPressed(Input.Keys.ENTER))
+        {
+            layer.getCell(x, y).setTile(shrubTiles.get(0));
+        }
+    }
+
+
+    private void animateTiles()
+    {
+        streamTilesH = new Array<StaticTiledMapTile>(2);
+        streamTilesV = new Array<StaticTiledMapTile>(2);
+        streamTilesUL = new Array<StaticTiledMapTile>(2);
+        streamTilesUR = new Array<StaticTiledMapTile>(2);
+        streamTilesDL = new Array<StaticTiledMapTile>(2);
+        streamTilesDR = new Array<StaticTiledMapTile>(2);
+        shrubTiles = new Array<StaticTiledMapTile>(2);
+
+
+
+        tiles = map.getTileSets().getTileSet(0).iterator();
+        while (tiles.hasNext())
+        {
+            TiledMapTile tile = tiles.next();
+
+            if (tile.getProperties().containsKey("Animation"))
+            {
+                if (tile.getProperties().get("Animation", String.class).equals("StreamH"))
+                {
+                    streamTilesH.add((StaticTiledMapTile)tile);
+                }
+                else if (tile.getProperties().get("Animation", String.class).equals("StreamV"))
+                {
+                    streamTilesV.add((StaticTiledMapTile)tile);
+                }
+                else if (tile.getProperties().get("Animation", String.class).equals("StreamUL"))
+                {
+                    streamTilesUL.add((StaticTiledMapTile)tile);
+                }
+                else if (tile.getProperties().get("Animation", String.class).equals("StreamUR"))
+                {
+                    streamTilesUR.add((StaticTiledMapTile)tile);
+                }
+                else if (tile.getProperties().get("Animation", String.class).equals("StreamDL"))
+                {
+                    streamTilesDL.add((StaticTiledMapTile)tile);
+                }
+                else if (tile.getProperties().get("Animation", String.class).equals("StreamDR"))
+                {
+                    streamTilesDR.add((StaticTiledMapTile)tile);
+                }
+                else if (tile.getProperties().get("Animation", String.class).equals("Shrub"))
+                {
+                    shrubTiles.add((StaticTiledMapTile)tile);
+                }
+            }
+        }
+
+        streamTilesUL.reverse();
+        streamTilesUR.reverse();
+        streamTilesDL.reverse();
+        streamTilesDR.reverse();
+
+        animatedStreamTileH = new AnimatedTiledMapTile(1/3f, streamTilesH);
+        animatedStreamTileV = new AnimatedTiledMapTile(1/3f, streamTilesV);
+        animatedStreamTileUL = new AnimatedTiledMapTile(1/3f, streamTilesUL);
+        animatedStreamTileUR = new AnimatedTiledMapTile(1/3f, streamTilesUR);
+        animatedStreamTileDL = new AnimatedTiledMapTile(1/3f, streamTilesDL);
+        animatedStreamTileDR = new AnimatedTiledMapTile(1/3f, streamTilesDR);
+        animatedShrubTile = new AnimatedTiledMapTile(1/3f, shrubTiles);
+
+        animatedStreamTileH.getProperties().put("Blocked", 0);
+        animatedStreamTileV.getProperties().put("Blocked", 0);
+        animatedStreamTileUL.getProperties().put("Blocked", 0);
+        animatedStreamTileUR.getProperties().put("Blocked", 0);
+        animatedStreamTileDL.getProperties().put("Blocked", 0);
+        animatedStreamTileDR.getProperties().put("Blocked", 0);
+
+        animatedStreamTileV.getProperties().put("Jumpable", 0);
+        animatedStreamTileH.getProperties().put("Jumpable", 0);
+
+
+        TiledMapTileLayer layer = (TiledMapTileLayer)map.getLayers().get(0);
+
+        for (int x = 0; x < layer.getWidth(); x++)
+        {
+            for (int y = 0; y < layer.getHeight(); y++)
+            {
+                TiledMapTileLayer.Cell cell = layer.getCell(x, y);
+
+                if (cell.getTile().getProperties().containsKey("Animation"))
+                {
+                    if (cell.getTile().getProperties().get("Animation",
+                            String.class).equals("StreamH"))
+                    {
+                        cell.setTile(animatedStreamTileH);
+                    }
+                    else if (cell.getTile().getProperties().get("Animation",
+                            String.class).equals("StreamV"))
+                    {
+                        cell.setTile(animatedStreamTileV);
+                    }
+                    else if (cell.getTile().getProperties().get("Animation",
+                            String.class).equals("StreamUL"))
+                    {
+                        cell.setTile(animatedStreamTileUL);
+                    }
+                    else if (cell.getTile().getProperties().get("Animation",
+                            String.class).equals("StreamUR"))
+                    {
+                        cell.setTile(animatedStreamTileUR);
+                    }
+                    else if (cell.getTile().getProperties().get("Animation",
+                            String.class).equals("StreamDL"))
+                    {
+                        cell.setTile(animatedStreamTileDL);
+                    }
+                    else if (cell.getTile().getProperties().get("Animation",
+                            String.class).equals("StreamDR"))
+                    {
+                        cell.setTile(animatedStreamTileDR);
+                    }
+                }
+            }
         }
     }
 
@@ -90,43 +271,69 @@ public class GameScreen implements Screen
 
         if (facing == 0)
         {
-            if (collisionLayer.getCell(xright/16,
-                    (yup + 1)/16).getTile().getProperties().containsKey("Blocked")
-                || collisionLayer.getCell((xleft + 1)/16,
-                    (yup + 1)/16).getTile().getProperties().containsKey("Blocked"))
+            TiledMapTileLayer.Cell topLeft = collisionLayer.getCell((xleft + 1)/16, (yup + 1)/16);
+            TiledMapTileLayer.Cell topRight = collisionLayer.getCell(xright/16, (yup + 1)/16);
+            if (topLeft.getTile().getProperties().containsKey("Jumpable")
+                    && topRight.getTile().getProperties().containsKey("Jumpable")
+                    && player.getJumpFrames() > 0)
+            {
+                return true;
+            }
+            else if (topLeft.getTile().getProperties().containsKey("Blocked")
+                || topRight.getTile().getProperties().containsKey("Blocked"))
             {
                 return false;
             }
         }
         else if (facing == 1)
         {
-            if (collisionLayer.getCell((xright+ 1)/16,
-                    yup/16).getTile().getProperties().containsKey("Blocked")
-                || collisionLayer.getCell((xright + 1)/16,
-                    (ydown + 1)/16).getTile().getProperties().containsKey("Blocked"))
+            TiledMapTileLayer.Cell topRight = collisionLayer.getCell((xright + 1)/16, yup/16);
+            TiledMapTileLayer.Cell bottomRight = collisionLayer.getCell((xright + 1)/16, (ydown + 1)/16);
+
+            if (topRight.getTile().getProperties().containsKey("Jumpable")
+                && bottomRight.getTile().getProperties().containsKey("Jumpable")
+                    && player.getJumpFrames() > 0)
+            {
+                return true;
+            }
+            else if (topRight.getTile().getProperties().containsKey("Blocked")
+                || bottomRight.getTile().getProperties().containsKey("Blocked"))
             {
                 return false;
             }
         }
         else if (facing == 2)
         {
-            if (collisionLayer.getCell((xleft + 1)/16,
-                    ydown/16).getTile().getProperties().containsKey("Blocked")
-                || collisionLayer.getCell(xright/16,
-                    ydown/16).getTile().getProperties().containsKey("Blocked"))
+            TiledMapTileLayer.Cell bottomLeft = collisionLayer.getCell((xleft + 1)/16, (ydown - 0)/16);
+            TiledMapTileLayer.Cell bottomRight = collisionLayer.getCell(xright/16, (ydown - 0)/16);
+            if (bottomLeft.getTile().getProperties().containsKey("Jumpable")
+                && bottomRight.getTile().getProperties().containsKey("Jumpable")
+                && player.getJumpFrames() > 0)
+            {
+                return true;
+            }
+            else if (bottomLeft.getTile().getProperties().containsKey("Blocked")
+                || bottomRight.getTile().getProperties().containsKey("Blocked"))
             {
                 return false;
             }
         }
         else // facing == 3
         {
-            if (collisionLayer.getCell(xleft/16,
-                    yup/16).getTile().getProperties().containsKey("Blocked")
-                || collisionLayer.getCell(xleft/16,
-                    (ydown + 1)/16).getTile().getProperties().containsKey("Blocked"))
+            TiledMapTileLayer.Cell topLeft = collisionLayer.getCell((xleft + 0)/16, yup/16);
+            TiledMapTileLayer.Cell bottomLeft = collisionLayer.getCell((xleft + 0)/16, (ydown + 1)/16);
+            if (topLeft.getTile().getProperties().containsKey("Jumpable")
+                    && bottomLeft.getTile().getProperties().containsKey("Jumpable")
+                    && player.getJumpFrames() > 0)
+            {
+                return true;
+            }
+            else if (topLeft.getTile().getProperties().containsKey("Blocked")
+                || bottomLeft.getTile().getProperties().containsKey("Blocked"))
             {
                 return false;
             }
+
         }
 
         return true;
@@ -191,6 +398,16 @@ public class GameScreen implements Screen
             player.translate(0f, -yChange);
             updateCamera(0f, -yChange);
         }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+        {
+            player.movementSpeed = 100;
+        }
+        else
+        {
+            player.movementSpeed = 70;
+        }
+
     }
 
     private void updateCamera(float xChange, float yChange)
--- core/src/com/idk/Player.java
+++ core/src/com/idk/Player.java
@@ -16,10 +16,11 @@ public class Player
     float movementSpeed;
     int facing; // 0 N, 1 E, 2 S, 3 W
     Texture playerTexture;
-    TextureAtlas boyAtlas;
+    TextureAtlas playerAtlas;
     Animation<TextureRegion> playerAnimation;
 
     private float elapsedTime = 0f;
+    private int jumpFrames = 0;
 
 
     public Player(float xCenter, float yCenter, float width, float height, float movementSpeed,
@@ -30,39 +31,73 @@ public class Player
         this.facing = facing;
     }
 
-    public void draw(Batch batch, float deltaTime)
+    public int getJumpFrames()
+    {
+        return jumpFrames;
+    }
+
+    public void draw(Batch batch, float deltaTime, float speed)
     {
         elapsedTime += deltaTime;
-        if (Gdx.input.isKeyPressed(Input.Keys.A))
+        float frameSpeed = (60/speed)*4;
+
+        if (jumpFrames > 0)
+        {
+            drawJump(batch, jumpFrames, this.facing);
+            jumpFrames--;
+        }
+        else if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
+        {
+            if (facing == 0)
+            {
+                playerTexture = new Texture("ManFaceUp.png");
+            }
+            else if (facing == 1)
+            {
+                playerTexture = new Texture("ManFaceRight.png");
+            }
+            else if (facing == 2)
+            {
+                playerTexture = new Texture("ManFaceDown.png");
+            }
+            else //facing == 3
+            {
+                playerTexture = new Texture("ManFaceLeft.png");
+            }
+
+            batch.draw(playerTexture, boundingBox.x, boundingBox.y);
+            jumpFrames = 40;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.A))
         {
             facing = 3;
-            boyAtlas = new TextureAtlas("ManWalkLeft.atlas");
-            playerAnimation = new Animation<TextureRegion>(1f/30f, boyAtlas.getRegions());
-            batch.draw(playerAnimation.getKeyFrame(elapsedTime / 4, true),
+            playerAtlas = new TextureAtlas("ManWalkLeft.atlas");
+            playerAnimation = new Animation<TextureRegion>(1f/30f, playerAtlas.getRegions());
+            batch.draw(playerAnimation.getKeyFrame(elapsedTime / frameSpeed, true),
                     boundingBox.x, boundingBox.y);
         }
         else if (Gdx.input.isKeyPressed(Input.Keys.D))
         {
             facing = 1;
-            boyAtlas = new TextureAtlas("ManWalkRight.atlas");
-            playerAnimation = new Animation<TextureRegion>(1f/30f, boyAtlas.getRegions());
-            batch.draw(playerAnimation.getKeyFrame(elapsedTime / 4, true),
+            playerAtlas = new TextureAtlas("ManWalkRight.atlas");
+            playerAnimation = new Animation<TextureRegion>(1f/30f, playerAtlas.getRegions());
+            batch.draw(playerAnimation.getKeyFrame(elapsedTime / frameSpeed, true),
                 boundingBox.x, boundingBox.y);
         }
         else if (Gdx.input.isKeyPressed(Input.Keys.S))
         {
             facing = 2;
-            boyAtlas = new TextureAtlas("ManWalkDown.atlas");
-            playerAnimation = new Animation<TextureRegion>(1f/30f, boyAtlas.getRegions());
-            batch.draw(playerAnimation.getKeyFrame(elapsedTime / 4, true),
+            playerAtlas = new TextureAtlas("ManWalkDown.atlas");
+            playerAnimation = new Animation<TextureRegion>(1f/30f, playerAtlas.getRegions());
+            batch.draw(playerAnimation.getKeyFrame(elapsedTime / frameSpeed, true),
                     boundingBox.x, boundingBox.y);
         }
         else if (Gdx.input.isKeyPressed(Input.Keys.W))
         {
             facing = 0;
-            boyAtlas = new TextureAtlas("ManWalkUp.atlas");
-            playerAnimation = new Animation<TextureRegion>(1f/30f, boyAtlas.getRegions());
-            batch.draw(playerAnimation.getKeyFrame(elapsedTime / 4, true),
+            playerAtlas = new TextureAtlas("ManWalkUp.atlas");
+            playerAnimation = new Animation<TextureRegion>(1f/30f, playerAtlas.getRegions());
+            batch.draw(playerAnimation.getKeyFrame(elapsedTime / frameSpeed, true),
                     boundingBox.x, boundingBox.y);
         }
         else
@@ -89,6 +124,30 @@ public class Player
             }
         }
 
+    }
+
+    public void drawJump(Batch batch, int jumpFrames, int facing)
+    {
+        if (facing == 0)
+        {
+            playerAtlas = new TextureAtlas("ManJU.atlas");
+        }
+        else if (facing == 1)
+        {
+            playerAtlas = new TextureAtlas("ManJR.atlas");
+        }
+        else if (facing == 2)
+        {
+            playerAtlas = new TextureAtlas("ManJD.atlas");
+        }
+        else // facing == 3
+        {
+            playerAtlas = new TextureAtlas("ManJL.atlas");
+        }
+
+        playerAnimation = new Animation<TextureRegion>(1, playerAtlas.getRegions());
+        batch.draw(playerAnimation.getKeyFrame(jumpFrames/4, true),
+                boundingBox.x, boundingBox.y);
     }
 
     public void translate(float xChange, float yChange)
